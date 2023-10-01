d18c3d2a446144ed55294f1fd10d96862d6af808
Pulotodom <61219276+turtle85917@users.noreply.github.com>
2023-10-01 13:50:00 +0900

    feat: implement look at player

--- Assets/Scripts/Game/Enemy.cs
+++ Assets/Scripts/Game/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -5,6 +6,7 @@ public class Enemy : MonoBehaviour
     public EnemyData enemy;
     private Animator animator;
     private Rigidbody2D Rigidbody;
+    [SerializeField] private GameObject Head;
     [Header("캐릭터 파츠")]
     [SerializeField] private SpriteRenderer headSprite;
     [SerializeField] private SpriteRenderer bodySprite;
@@ -21,5 +23,22 @@ public class Enemy : MonoBehaviour
     }
 
     private void Update()
-    {}
+    {
+        LookAtPlayer();
+    }
+
+    private void LookAtPlayer()
+    {
+        Vector3 playerPosition = Player.instance.transform.position;
+        Vector2 distance = transform.position.x < playerPosition.x
+            ? playerPosition - transform.position
+            : transform.position - playerPosition
+        ;
+        float angle = (float)(Math.Atan2(distance.y, distance.x) * 180 / Math.PI);
+        headSprite.flipX = transform.position.x > playerPosition.x;
+        if(Math.Abs(angle) < 80)
+            Head.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        else
+            Head.transform.rotation = Quaternion.identity;
+    }
 }
