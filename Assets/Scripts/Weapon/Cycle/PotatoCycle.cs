305ece17987107b333d1a95e66dfbbb31fc3d791
Pulotodom <61219276+turtle85917@users.noreply.github.com>
2023-11-04 17:40:27 +0900

    fix: potato weapon using player

--- Assets/Scripts/Weapon/Cycle/PotatoCycle.cs
+++ Assets/Scripts/Weapon/Cycle/PotatoCycle.cs
@@ -12,7 +12,10 @@ public class PotatoCycle : BaseCycle
             yield return new WaitForSeconds(weapon.stats.Cooldown);
             GameObject potato = ObjectPool.Get(Game.PoolManager, "Potato", (GameObject)weapon.weapon.resources[0]);
             float cameraHeight = Camera.main.orthographicSize;
-            potato.transform.position = new Vector3(weaponUser.transform.position.x + -9.265f, weaponUser.transform.position.y + Random.Range(0.3f, cameraHeight + Camera.main.transform.position.y));
+            Vector3 position = new Vector3(weaponUser.transform.position.x + -9.265f, weaponUser.transform.position.y + Random.Range(0.3f, cameraHeight + Camera.main.transform.position.y));
+            if(weaponUser.CompareTag("Enemy"))
+                position = weaponUser.transform.position;
+            potato.transform.position = position;
             Potato script = potato.GetComponent<Potato>();
             script.stats = weapon.stats;
             script.weaponUserType = GameUtils.GetWeaponUserType(weaponUser);
--- Assets/Scripts/Weapon/Projectile/Potato.cs
+++ Assets/Scripts/Weapon/Projectile/Potato.cs
@@ -21,7 +21,7 @@ public class Potato : BaseWeapon
         if(weaponStatus == WeaponStatus.Idle)
         {
             var targets = Scanner.ScanAll(transform.position, stats.Range, GameUtils.GetTargetTag(weaponUserType), stats.Penetrate);
-            if(targets.Count > 1)
+            if(targets.Count > 0)
             {
                 rigid.velocity = Vector2.zero;
                 weaponStatus = WeaponStatus.GoAway;
