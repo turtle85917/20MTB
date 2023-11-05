e4e6289dbb4f313bd10eb46a193ce45d9d5db89d
Pulotodom <61219276+turtle85917@users.noreply.github.com>
2023-11-05 21:28:36 +0900

    feat: implement critical, balancing

--- Assets/Scripts/Game/AttackManager.cs
+++ Assets/Scripts/Game/AttackManager.cs
@@ -1,4 +1,5 @@
 using System;
+using _20MTB.Stats;
 using UnityEngine;
 
 public static class AttackManager
@@ -14,42 +15,61 @@ public static class AttackManager
     public static void AttackTarget(string weaponId, GameObject target, int penetrate, Action<Affecter> postProcessFunc = null, GameObject source = null)
     {
         Weapon weapon = WeaponBundle.GetWeapon(weaponId);
+
+#region 공격 대상/무기 사용자가 적일 경우, 적 데이터 가져오기
         EnemyManager.EnemyPool enemyPool = null;
-        int power = Player.playerData.data.stats.Power;
-        if(source && source.CompareTag("Enemy"))
-        {
-            enemyPool = EnemyManager.GetEnemy(source);
-            power = enemyPool.data.stats.Power;
-        }
-        int damage = weapon.stats.Power + weapon.stats.Power / power - penetrate * weapon.stats.DecreasePower;
-        if(enemyPool != null) damage = enemyPool.weapon.stats.Power + enemyPool.weapon.stats.Power / power - penetrate * enemyPool.weapon.stats.DecreasePower;
+        EnemyManager.EnemyPool sourceEnemyPool = null;
+        if(target.CompareTag("Enemy")) enemyPool = EnemyManager.GetEnemy(target);
+        if(source && source.CompareTag("Enemy")) sourceEnemyPool = EnemyManager.GetEnemy(source);
+#endregion
+
+        bool critical = UnityEngine.Random.value < (target.CompareTag("Enemy") ? weapon.stats.CriticalHit : sourceEnemyPool.weapon.stats.CriticalHit);
+        int damage = GetCalcDamage(Player.playerData.data.stats.Power, GetWeaponPower(critical, weapon.stats), penetrate * weapon.stats.DecreasePower);
+        if(sourceEnemyPool != null) damage = GetCalcDamage(sourceEnemyPool.data.stats.Power, GetWeaponPower(critical, sourceEnemyPool.weapon.stats), penetrate * sourceEnemyPool.weapon.stats.DecreasePower);
         if(target.CompareTag("Player"))
         {
             Weapon parasocialWeapon = Player.playerData.weapons.Find(item => item.weapon.weaponId == "パラソーシャル");
             if(parasocialWeapon != null && UnityEngine.Random.value < 0.4f && source != null)
             {
-                enemyPool = EnemyManager.GetEnemy(source);
-                damage = Mathf.RoundToInt(damage * 0.25f);
+                sourceEnemyPool = EnemyManager.GetEnemy(source);
+                // 크리티컬 데미지가 아닌 값으로 처리하기 위함
+                damage = Mathf.RoundToInt(GetCalcDamage(sourceEnemyPool.data.stats.Power, sourceEnemyPool.weapon.stats.Power, penetrate * sourceEnemyPool.weapon.stats.DecreasePower) * 0.25f);
                 if(damage > 0)
                 {
-                    enemyPool.health -= damage;
-                    TextManager.WriteDamage(enemyPool.target, damage, false);
+                    sourceEnemyPool.health -= damage;
+                    TextManager.WriteDamage(sourceEnemyPool.target, damage, false);
                 }
             }
             else
             {
                 Player.playerData.health -= damage;
-                TextManager.WriteDamage(Player.@object, damage, false);
+                TextManager.WriteDamage(Player.@object, damage, critical);
             }
         }
         else
         {
             enemyPool.health -= damage;
-            TextManager.WriteDamage(target, damage, false);
+            TextManager.WriteDamage(target, damage, critical);
         }
         if(postProcessFunc != null)
         {
             postProcessFunc(target.GetComponent<Affecter>());
         }
+    }
+
+    private static int GetWeaponPower(bool critical, WeaponStats stats)
+    {
+        return critical ? stats.CriticalDamage : stats.Power;
+    }
+
+    /// <summary>
+    /// 계산된 데미지를 반환합니다.
+    /// </summary>
+    /// <param name="characterPower">공격하는 대상의 힘</param>
+    /// <param name="weaponPower">무기의 공격력</param>
+    /// <param name="decreasePower">최종 값에서 깎이는 데미지</param>
+    private static int GetCalcDamage(int characterPower, int weaponPower, int decreasePower)
+    {
+        return weaponPower + weaponPower / characterPower - decreasePower;
     }
 }
